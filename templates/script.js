a450edd77cdc7f75e5081d8d653e7db888c27bb5
Miguel Angel Ruiz Micó <145917465+miguelrm81@users.noreply.github.com>
2023-12-17 20:19:20 +0100

    Add files via upload

--- templates/script.js
+++ templates/script.js
@@ -1,9 +1,43 @@
 function consultarAPI() {
     var pregunta = document.getElementById('question').value;
     var respuestaContainer = document.getElementById('respuesta');
-    // Aqui tengo que poner la funcion de los resultados de la api
-    var respuestaDeLaAPI = "Respuesta de la API para: '" + pregunta + "'";
 
-    // Mostrar la respuesta en el contenedor
-    respuestaContainer.innerHTML = respuestaDeLaAPI;
+    // Guardar la pregunta en un archivo JSON utilizando fetch
+    guardarPreguntaEnJSON(pregunta)
+        .then(() => {
+            // Luego de guardar la pregunta, puedes realizar la consulta a tu API
+            // Aquí tendrías tu lógica para consultar la API y mostrar la respuesta
+            var respuestaDeLaAPI = "Respuesta de la API para: '" + pregunta + "'";
+
+            // Mostrar la respuesta en el contenedor
+            respuestaContainer.innerHTML = respuestaDeLaAPI;
+        })
+        .catch(error => {
+            console.error('Error al guardar la pregunta:', error);
+            respuestaContainer.innerHTML = 'Error al guardar la pregunta.';
+        });
+}
+
+function guardarPreguntaEnJSON(pregunta) {
+    // URL de tu endpoint en el servidor Flask para guardar la pregunta
+    var guardarPreguntaURL = '/insert';
+
+    // Configurar los datos a enviar en la solicitud
+    var data = {
+        question: pregunta
+    };
+
+    // Configurar la solicitud POST
+    return fetch(guardarPreguntaURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error al guardar la pregunta en el servidor.');
+        }
+    });
 }
